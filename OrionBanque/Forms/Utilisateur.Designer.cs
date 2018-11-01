10f92b04a3f64db2307155cae321fe9082241c9c
Cyril CHARLIER <cyril.charlier@gmail.com>
2018-11-01 11:40:23 +0100

    Bouton par défaut fenêter utilisateur

--- OrionBanque/Forms/Utilisateur.Designer.cs
+++ OrionBanque/Forms/Utilisateur.Designer.cs
@@ -143,6 +143,7 @@
             // 
             // Utilisateur
             // 
+            this.AcceptButton = this.OK;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.CancelButton = this.Fermer;
